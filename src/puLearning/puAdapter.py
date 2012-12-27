fbb43abf901494bf24eba1aab0900e1d4a9f0db2
Alexandre Drouin <alexandre.drouin1@gmail.com>
2012-12-27 15:10:42 -0500

    support for precomputed kernel matrix

--- src/puLearning/puAdapter.py
+++ src/puLearning/puAdapter.py
@@ -17,7 +17,7 @@ class PUAdapter(object):
     '''
 
 
-    def __init__(self, estimator):
+    def __init__(self, estimator, precomputed_kernel=False):
         '''
         Constructor
         
@@ -26,18 +26,71 @@ class PUAdapter(object):
                        and outputs p(s=1|x) for each feature vector
                      * a fit(X, y) method which takes a list of feature vectors and
                        a list of their associated labels
+        precomputed_kernel -- Specifies if the matrices provided in the fit and predict
+                              methods are feature vectors or a precomputed kernel matrix.
         '''
         self.estimator = estimator
         self.c = 1.0
+        self.precomputed_kernel = precomputed_kernel
         
         
     def __str__(self):
         desc = 'Estimator:' + str(self.estimator) + '\n'
         desc += 'p(s=1|y=1,x): c=' + str(self.c)
         return desc
-        
-        
+    
+    
     def fit(self, X, y):
+        '''
+        Fits an estimator of p(s=1|x) and estimates the value of c=p(s=1|y=1,x)
+        
+        X -- List of feature vectors
+        y -- Labels associated to each feature vector in X
+        '''
+        if self.precomputed_kernel:
+            return self.fit_precomputed_kernel(X,y)
+        else:
+            return self.fit_no_precomputed_kernel(X,y)
+    
+    
+    def fit_precomputed_kernel(self, X, y):
+        '''
+        Fits an estimator of p(s=1|x) and estimates the value of c=p(s=1|y=1,x)
+        
+        X -- List of feature vectors
+        y -- Labels associated to each feature vector in X
+        '''
+        #The following are indexes
+        positives = np.where(y == 1.)[0]
+
+        if len(positives) < 21:
+            raise('Not enough positive examples to estimate p(s=1|y=1,x)')
+        
+        np.random.shuffle(positives)
+        hold_out = positives[:20]
+        
+        #Hold out test kernel matrix
+        X_test_hold_out = X[hold_out]
+        keep = list(set(np.arange(len(y))) - set(hold_out))
+        X_test_hold_out = X_test_hold_out[:,keep]
+        
+        #New training kernel matrix
+        X = X[:, keep]
+        X = X[keep]
+
+        y = np.delete(y, hold_out)
+        
+        #We fit the estimator
+        self.estimator.fit(X, y)
+        
+        #We estimate c using Elkan's estimator e_1
+        pred_probas = self.estimator.predict_proba(X_test_hold_out)[:,1]
+        #c is estimated as the average p(s=1|x) of all positive examples
+        c = np.mean(pred_probas)
+        print "p(s=1|y=1,x): ", c
+        self.c = c
+        
+    def fit_no_precomputed_kernel(self, X, y):
         '''
         Fits an estimator of p(s=1|x) and estimates the value of c=p(s=1|y=1,x)
         
